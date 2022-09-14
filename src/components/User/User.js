86be9b8ada6c4e171b2831856d7868e19900d667
Oleh3131 <oshevskyioleh@gmail.com>
2022-09-14 18:12:04 +0300

    add info to second task of homework

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,8 +3,9 @@ import React from 'react';
 import './User.css'
 
 const User = ({user:{id,name,status,species,gender,image}}) => {
+
     return (
-        <div className={'UserBlock'}>
+        <div className={'UserBlock'} onClick={() => window.open("https://www.imdb.com/title/tt2861424/episodes")}>
 
             <h2>id: {id}</h2>
             <h2>name: {name}</h2>

f7e9d159e06767e3885904e1de1cb2abc7906d57
Oleh3131 <oshevskyioleh@gmail.com>
2022-11-12 23:21:00 +0300

    registration and authentication

---
+++ src/configs/urls.js
@@ -0,0 +1,21 @@
+const baseURL = 'https://owu.linkpc.net/api/v3';
+
+const urls={
+
+    cars:'/cars',
+    auth:{
+
+        loin:'/auth',
+        refresh:'/auth/refresh'
+
+    },
+    users: '/users'
+
+}
+
+export {
+
+    baseURL,
+    urls
+
+}
